d0ed8cb9ec475b7520fef60380ef57be19e3b3c3
Denys Shevel <denys.shevel16@gmail.com>
2022-07-16 06:32:25 -0400

    Add additional exports

--- src/export.ts
+++ src/export.ts
@@ -1,10 +1,18 @@
 export { ApiResult } from './queryHandlers';
 export { default as Instance } from './Instance';
-export { type default as EntityBlurprint } from './EntityBlueprint';
 export * as errors from './errors';
 export { string, number, boolean, array, entity } from './entityMembers';
-export {
-  type Include,
-  type default as EntityBlueprint,
+export type { DataModifierArgs, WithId } from './dataModifier';
+export type {
+  Include,
+  Fetcher,
+  Creator,
+  Deleter,
+  Mutator,
+  EntityMember,
+  BlueprintsDict,
+  CreatorArgs,
+  FetcherArgs,
+  MutatorArgs,
+  default as EntityBlueprint,
 } from './EntityBlueprint';
-export { Creator, Fetcher, Deleter } from './EntityBlueprint';
